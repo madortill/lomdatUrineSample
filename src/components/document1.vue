f095e86d91f279abf39ff386883504550d5d7c5a
madortill <mador.till@gmail.com>
2025-12-25 11:35:55 +0200

    כןרדא3

--- src/components/document1.vue
+++ src/components/document1.vue
@@ -6,9 +6,20 @@
     <div v-if="result !== ''" class="black-div"></div>
     <document class="document1" @result="sendResult" @to-end="toEnd"></document>
     <result class="result" :resultFrom="result" />
-    <button class="back-btn" @click="$emit('back-to-info')">
-  חזרה לפרטים
+    <button class="back-btn" @click="openDit = !openDit">
+   פתיחת פרטים
 </button>
+<div v-if="openDit" class="text-div">
+      <p class="text">חשוד - רב״ט לירון אסולין מ.א- 9543874 ת.ז-256333791 , מאבטח מתקנים בבא״ף 8223 
+חשדות - סחר בסמים מסוכנים, שימוש והחזקה בסמים מסוכנים
+בלש - סמל רוני בן משה מ.א- 8357012 בלש מצ״ח במצ״ח 6012
+סימוכין - מס׳ תיק- 2025/0037 מתנהל במצ״ח יואב כאשר סימול הבסיס הינו 3</p>
+      <p class="text">במצ״ח יואב התקבל דיווח המחשיד את רב״ט לירון אסולין בסחר בסמים מסוכנים, שימוש והחזקה בסמים מסוכנים מסוג ״קנאביס״ במהלך שירותו הצבאי בקרב מספר חיילים בבסיסו הממוקם בבא״ף לכיש. 
+        החשוד נחקר אודות החשדות הנ״ל בתאריך 01 במרץ 2025, במהלך החקירה החשוד הסכים לביצוע בדיקת שתן, טרם כך יש להחתים אותו על טופס הסכמה על פי השלבים הבאים:</p>
+      <p class="text">חלק א׳- הסכמה חופשית של החשוד על עצם נטילת דגימת השתן
+חלק ב׳- נטילת דגימת השתן עצמה על פי העקרונות המוסכמים 
+חלק ג׳- הצגת תוצאות הבדיקה בפני החשוד כאשר הם התקבלו חיוביות לסם מסוכן מסוג ״קנאביס״</p>
+    </div>
   </div>
 </template>
 
@@ -26,6 +37,7 @@ export default {
       reasonAnswer: "",
       roomUser: "",
       result: "",
+      openDit: false,
     };
   },
   methods: {
@@ -75,6 +87,22 @@ export default {
   position: fixed;
   right: 1%;
   z-index: 3;
+}
+.text-div {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  background-color: #be0000;
+  padding: 1rem;
+  border-radius: 2rem;
+}
+.text {
+  color: white;
+  width: 20rem;
+  font-family: "rubik";
+
 }
 @media (max-width: 630px) {
   .document1 {
